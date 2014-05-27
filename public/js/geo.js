82c3f4803f4b020575f60c89a07a087da0d80ccf
Ian Kynnersley <ian@iankynnersley.co.uk>
2014-05-27 19:18:34 +0100

    Layout and JS improvements

--- public/js/geo.js
+++ public/js/geo.js
@@ -14,32 +14,31 @@ if (navigator.geolocation) {
 //     L.circle([story.centre.latitude, story.centre.longitude], story.radius).addTo(map);
 //     markers.push(marker);
 //   });
-  
+
 //   var group = new L.featureGroup(markers);
 //   map.fitBounds(group.getBounds());
 // });
 
-var story_template = '' + 
-'<div class="story" data-story-id="{{id}}">' +
-'  <h2 class="name">{{name}}</h2>' +
-'  <div class="controls">' +
-'    <a href="#"><i class="fa fa-play-circle play" /></a>' +
-'  </div>' +
-'  <h3 class="author">by <span class="author-name">William Sutcliffe</span></h3>' +
-'  <p class="published">on <span class="published-date">23rd Feb 2014</span></p>' +
-'</div>';
-
 map.on('locationfound', function(e) {
   console.log( 'position: ' + e.latlng.lat + ', ' + e.latlng.lng );
 
+  // Best solution is probably to GET /stories/lat,lng here passing in user's location
+  // that should then return the markup for the found story.
+  // This means that the logic to determine if someone can listen to a story happens on the server.
+
+  // Fetch the polygons and markers for the stories
   $.getJSON('http://api.tiles.mapbox.com/v3/kpopper.hkk51f45/markers.geojson', function(data) {
     console.log('polygons retrieved: ' + JSON.stringify(data));
+
+    // polygons will contain an array of polygons the current user is inside
     var geojson = L.geoJson(data);
     var polygons = leafletPip.pointInLayer([e.latlng.lng, e.latlng.lat], geojson);
     console.log(polygons);
   });
 
   $.getJSON('/stories/' + e.latlng.lat + ',' + e.latlng.lng, function(data){
+    // Perhaps this should return an oEmbed format document?
+
     if( data.lenth == 0 ) {
       $( "#no-stories" ).show();
       $( "#stories" ).empty().hide();
